c99c64a127d1ac1d3153164c7f79ae76fcd662aa
unknown <huangetienne123@gmail.com>
2024-03-10 03:27:37 +0100

    problème avec update

--- reservations/catalogue/views/artist.py
+++ reservations/catalogue/views/artist.py
@@ -6,9 +6,11 @@ from catalogue.models import Artist
 # Create your views here.
 def index(request):
 	artists = Artist.objects.all()
+	title = 'Liste des artistes'
 	
 	return render(request, 'artist/index.html', {
 		'artists':artists,
+		'title':title
 	})
 
 def show(request, artist_id):
@@ -17,6 +19,22 @@ def show(request, artist_id):
 	except Artist.DoesNotExist:
 		raise Http404('Artist inexistant');
 		
+	title = 'Fiche d\'un artiste'
+	
 	return render(request, 'artist/show.html', {
 		'artist':artist,
+		'title':title 
+	})
+ 
+def edit(request, artist_id):
+	try:
+		artist = Artist.objects.get(id=artist_id)
+	except Artist.DoesNotExist:
+		raise Http404('Artist inexistant');
+		
+	title = 'Fiche d\'un artiste'
+	
+	return render(request, 'artist/edit.html', {
+		'artist':artist,
+		'title':title 
 	})
